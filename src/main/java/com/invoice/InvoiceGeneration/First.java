28c83de9fc161b7d191f025a69588a415c97a183
GangarajuKR <gangagangaraju1@gmail.com>
2022-06-17 17:33:53 +0530

    Added

---
+++ src/main/java/com/invoice/InvoiceGeneration/First.java
@@ -0,0 +1,19 @@
+package com.invoice.InvoiceGeneration;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+public class First {
+	//creating the PDF
+	PDDocument MyPDF = new PDDocument();
+	
+	//Creating a Blank Page
+	PDPage newpage = new PDPage();
+	//Adding the blank page to our PDF
+	
+	
+	private void MyPDF() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/test/java/com/invoice/InvoiceGeneration/InvoiceGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoice.InvoiceGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
